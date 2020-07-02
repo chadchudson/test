2e6c1ed77bfc1613fd18285954104780f50944e6
Chad Hudson <chadchudson@gmail.com>
2020-07-02 17:19:09 +0000

    local changes

--- test.py
+++ test.py
@@ -5,3 +5,4 @@ print("Sweet, have a new server spun-up and seems to be working")
 name = input("What's your name? ")
 print("Welcome to Ubnt01 server, " + name)
 
+print("We think this is a great tool and are excited about using it.")
